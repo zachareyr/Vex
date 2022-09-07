363b620b47f83077d32be5f4928dfaa0650bfb73
zachareyr <zach.zacharyr@gmail.com>
2022-09-07 07:17:18 -0400

    Added wheel attributes

--- include/drivesystems/drive.hpp
+++ include/drivesystems/drive.hpp
@@ -5,4 +5,27 @@ extern pros::Motor m_topright;
 extern pros::Motor m_bottomleft;
 extern pros::Motor m_bottomright;
 
-#define STARTING_POSITION 
+
+// TODO measure this shit
+
+// Where the robot starts on the field (left side)
+#define STARTING_POSITION_1
+
+// Where the robot starts on the field (right side)
+#define STARTING_POSITION_2
+
+// Wheel attributes
+#define WHEEL_CIRCUMFERENCE
+#define WHEEL_RADIUS
+#define WHEEL_DIAMETER
+#define WHEEL_WIDTH
+
+// Distance between the axles
+#define WHEEL_BASE
+
+// Width of the robot  
+#define TRACK_WIDTH
+
+
+// ???
+#define mikey "9q0w9e-09403oer[p]"
